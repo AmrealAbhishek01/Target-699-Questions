6fd937273a30c3e2e751d012e9e9a0467844f45b
AmrealAbhishek01 <abhishek.kumar2021f@vitstudent.ac.in>
2023-09-02 13:45:56 +0530

    Pattern1

---
+++ Pattern1.cpp
@@ -0,0 +1,34 @@
+// Pattern-1: Rectangular Star Pattern
+// Problem Statement: Given an integer N, print the following pattern.
+
+// Example 1:
+// Input: N = 3
+// Output: 
+// * * *
+// * * *
+// * * *
+
+
+// Example 2:
+// Input: N = 6
+// Output:
+// * * * * * *
+// * * * * * *
+// * * * * * *
+// * * * * * *
+// * * * * * *
+// * * * * * *
+
+
+class Solution {
+  public:
+    void printSquare(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+    }
+};
